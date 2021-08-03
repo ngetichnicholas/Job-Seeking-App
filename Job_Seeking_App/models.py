65130e036b21e093f18ec3ea99d222f64b10fdac
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-08-03 10:02:35 +0300

    Jobseeker&Employer models inherit from custom user

--- Job_Seeking_App/models.py
+++ Job_Seeking_App/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractUser)
 
 # Create your models here.
+class User(AbstractUser):
+    is_jobseeker = models.BooleanField(default=False)
+    is_employer = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+
+
+class JobSeeker(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    phone = models.CharField(max_length=25)
+    location = models.CharField(max_length=25)
+
+
+class Employer(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    phone = models.CharField(max_length=15)
