1875557fad7f7d98e0fd60228698197ebef5618e
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-08-03 10:18:03 +0300

    email field added in custom user model

--- Job_Seeking_App/models.py
+++ Job_Seeking_App/models.py
@@ -7,14 +7,16 @@ class User(AbstractUser):
     is_employer = models.BooleanField(default=False)
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
+    email = models.EmailField(max_length=50,null=True)
 
 
 class JobSeeker(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
     phone = models.CharField(max_length=25)
-    location = models.CharField(max_length=25)
+    location = models.CharField(max_length=25,null=True)
 
 
 class Employer(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
     phone = models.CharField(max_length=15)
+    location = models.CharField(max_length=25,null=True)
