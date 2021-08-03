ae79362db3d725c364303f16b7d46aeb050c39eb
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-03 12:36:28 +0300

    Add employer signup view

---
+++ Job_Seeking_App/views/employer.py
@@ -0,0 +1,26 @@
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import (CreateView)
+
+from ..forms import  EmployerSignUpForm
+from ..models import  User
+
+
+class EmployerSignUpView(CreateView):
+    model = User
+    form_class = EmployerSignUpForm
+    template_name = 'registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'employer'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('employers:employer_dashboard')
