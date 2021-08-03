0c3b84499483322a6fff1f865b2aeeb1d7d2f3fb
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-03 13:35:48 +0300

    Update urls and views for employer and jobseeker

--- Job_Seeking_App/urls.py
+++ Job_Seeking_App/urls.py
@@ -1,1 +1,16 @@
 
+from django.urls import include, path
+
+from .views import jobseeking, jobseeker, employer
+
+urlpatterns = [
+    path('', jobseeking.home, name='home'),
+
+    path('jobseeker/', include(([
+        path('', jobseeker.JobSeekerDashboardView, name='jobseeker_dashboard'),
+    ], 'jobseeking'), namespace='jobseeker')),
+
+    path('employer/', include(([
+        path('', employer.EmployerDashboardView, name='employer_dashboard'),
+    ], 'jobseeking'), namespace='employer')),
+]
