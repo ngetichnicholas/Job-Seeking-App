e9859235b66b2fd0c30495b52638d1d82d5c4fdb
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-03 12:29:53 +0300

    Add Jobseeker Signup view

---
+++ Job_Seeking_App/views/jobseeker.py
@@ -0,0 +1,27 @@
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.db.models import Count
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, ListView, UpdateView
+
+from ..forms import  JobseekerSignUpForm
+from ..models import JobSeeker, User
+
+
+class JobseekerSignUpView(CreateView):
+    model = User
+    form_class = JobseekerSignUpForm
+    template_name = 'registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'jobseeker'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('jobseekers:jobseeker_dashboard')
