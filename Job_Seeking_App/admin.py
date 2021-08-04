c2f62a50d819f6d335d735d13aa411b3bc0e1709
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-08-04 18:02:26 +0300

    registered the category models

--- Job_Seeking_App/admin.py
+++ Job_Seeking_App/admin.py
@@ -1,7 +1,9 @@
-from Job_Seeking_App.models import Employer, JobSeeker, User
+from Job_Seeking_App.models import Employer, JobSeeker, Jobs, User,Category
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(JobSeeker)
 admin.site.register(Employer)
 admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Jobs)
