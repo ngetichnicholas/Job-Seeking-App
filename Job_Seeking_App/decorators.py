b770be3607ffcaa21a6ace1684e884c9cbfde0ad
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-08 15:48:27 +0300

    Protect admin views from being accessed by non-admin users

--- Job_Seeking_App/decorators.py
+++ Job_Seeking_App/decorators.py
@@ -9,3 +9,36 @@ def unauthenticated_user(view_func):
       return view_func(request, *args, **kwargs)
 
   return wrapper_func
+
+def allowed_users(allowed_roles=[]):
+  def decorator(view_func):
+    def wrapper_func(request, *args, **kwargs):
+      group = None
+      if request.user.groups.exists():
+        group = request.user.groups.all()[0].name
+
+      if group in allowed_roles:
+        return view_func(request, *args, **kwargs)
+
+      else:
+        return HttpResponse('You are not authorized to access this page')
+    return wrapper_func
+  return decorator
+
+def admin_only(view_func):
+  def wrapper_func(request, *args, **kwargs):
+    
+    group = None
+    if request.user.groups.exists():
+      group = request.user.groups.all()[0].name
+
+    if group == 'jobseeker':
+      return redirect('jobseekerDash')
+
+    if group == 'employer':
+      return redirect('employerDash')
+
+    if group == 'admin':
+      return view_func(request, *args, **kwargs)
+
+  return wrapper_func
