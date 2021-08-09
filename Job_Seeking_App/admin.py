8bc8b47e32d7d3c188e9921114072eba75678bfe
TechVictorKE <vicikibocha@gmail.com>
2021-08-09 09:24:19 +0300

    Modified forms

--- Job_Seeking_App/admin.py
+++ Job_Seeking_App/admin.py
@@ -1,9 +1,11 @@
-from Job_Seeking_App.models import Employer, JobSeeker, Jobs, User,Category
+from Job_Seeking_App.models import *
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(JobSeeker)
 admin.site.register(Employer)
 admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Jobs)
+# admin.site.register(Category)
+# admin.site.register(Jobs)
+admin.site.register(FileUpload)
+admin.site.register(Portfolio)
