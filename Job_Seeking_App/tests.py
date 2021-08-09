2d44b940e5428ba35b7862ceddb07801c499c8d7
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-08-09 12:35:52 +0300

    employer & portfolio models ready for testing

--- Job_Seeking_App/tests.py
+++ Job_Seeking_App/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Portfolio,Employer
 
 # Create your tests here.
+class EmployerTestCase(TestCase):
+    pass
+
+class PortfolioTestCase(TestCase):
+    pass
