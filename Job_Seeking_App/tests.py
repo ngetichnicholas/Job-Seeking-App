9d8de9f2e1007fc7dae96eda57b8192438c61069
Roney-juma <ochirone10@gmail.com>
2021-08-09 15:56:15 +0300

    Jobseeker testcase

--- Job_Seeking_App/tests.py
+++ Job_Seeking_App/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import JobSeeker
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class JobSeekerTest(TestCase):
+    
+  def setUp(self):
+    self.user = User.objects.create(id=1, username='Roney-juma')
