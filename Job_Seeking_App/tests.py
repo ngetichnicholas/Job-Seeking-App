238aaf3428b69e2ae701b4469a727b3858340129
annechege <annechege780@gmail.com>
2021-08-10 17:02:30 +0300

    testing user

--- Job_Seeking_App/tests.py
+++ Job_Seeking_App/tests.py
@@ -1,3 +1,16 @@
+from _typeshed import Self
+from Job_Seeking_App.apps import JobSeekingAppConfig
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+# class UserTestClass(TestCase):
+#     def setUp(self):
+#         post_object.create(title='test', content='something')
+
+
+#     def test_post_title{self}:
+#         post=post.objects.get{id=4}
+#         Self.assertEqual(post.title'title')
+
+    
