d4cbba15c954f417f44b0b6f01819315c89f39b1
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-11 16:17:30 +0300

    Add email confirmation for payment

--- mpesa/payment.py
+++ mpesa/payment.py
@@ -1,6 +1,13 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from django.http import HttpResponse,HttpResponseRedirect,Http404,JsonResponse
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib import messages
+from .email import send_payment_email
+from Job_Seeking_App.forms import *
+
+
+
 
 
 from .access_token import generate_access_token
@@ -12,6 +19,9 @@ from . import keys
 def mpesa_payment(request):
     user = request.user
     phone = request.POST.get('mpesa_number')
+    email = user.email
+    name = user.username
+    payment_form = PaymentForm()
     formatted_time = get_timestamp()
     decoded_password = create_password(formatted_time)
     access_token = generate_access_token()
@@ -26,18 +36,18 @@ def mpesa_payment(request):
         "Timestamp": formatted_time,
         "TransactionType": "CustomerPayBillOnline",
         "Amount": "1",
-        "PartyA": 254725470732,
+        "PartyA": phone,
         "PartyB": keys.business_shortCode,
         "PhoneNumber": phone,
         "CallBackURL": "https://job-seeking-app.herokuapp.com/api/payments/transaction/",
-        "AccountReference": "test aware",
-        "TransactionDesc": "Pay School Fees",
+        "AccountReference": "Jobseeker Agency",
+        "TransactionDesc": "This is for mpesa payment testing",
     }
 
     response = requests.post(api_url, json=request, headers=headers)
-
     print(response.text)
+    send_payment_email(name, email)
 
-    return HttpResponse('You will receive mpesa pop up, please enter your pin')
+    return HttpResponse('Mpesa push notification sent, enter your pin to confirm payment')
 
 
