30e8ef729bea7adfe77a5e8c0cf44d438541ecf9
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-12 13:55:04 +0300

    Add about, services, contact pages and create Contact model

--- Job_Seeking_App/views.py
+++ Job_Seeking_App/views.py
@@ -28,6 +28,15 @@ from .models import User
 def index(request):
     return render(request,'index.html')
 
+def about(request):
+    return render(request,'about.html')
+
+def services(request):
+    return render(request,'services.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
 #signup and login
 @unauthenticated_user
 def register(request):
