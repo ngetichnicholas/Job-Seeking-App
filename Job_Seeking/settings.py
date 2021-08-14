7eba214847a885cbd7c693f1e2c371bf3366ec3b
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-14 16:37:21 +0300

    Update mpesa

--- Job_Seeking/settings.py
+++ Job_Seeking/settings.py
@@ -108,8 +108,8 @@ DATABASES = {
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
