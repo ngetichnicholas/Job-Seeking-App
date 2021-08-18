3e9b13e37f1c41b5b258e3d754ec612c5ee9f40a
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-18 12:32:16 +0300

    Complete unit tests

--- mpesa/models.py
+++ mpesa/models.py
@@ -14,5 +14,21 @@ class Payment(models.Model):
     TransactionDate = models.DateTimeField(blank=True, null=True)
     PhoneNumber = models.CharField(max_length=13, blank=True, null=True)
 
+    def save_payment(self):
+        self.save()
+
+    def delete_payment(self):
+        self.delete()
+
+    @classmethod
+    def get_all_payments(cls):
+        payments = cls.objects.all()
+        return payments
+
+    @classmethod
+    def get_payment_id(cls,id):
+        payment_id = cls.objects.filter(id= id).all()
+        return payment_id
+
     def __str__(self):
         return f"{self.PhoneNumber} has sent {self.Amount} >> {self.MpesaReceiptNumber}"
