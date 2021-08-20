e8c90a5b7d475b4e76a27c8efd166fc929fcc26d
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-08-20 04:44:24 +0300

    Deployment

--- mpesa/api/urls.py
+++ mpesa/api/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from mpesa.api.views import CallBackApiView
+from Job_Seeking_App import views as app_views
 
 urlpatterns = [
     path("transaction/", CallBackApiView.as_view(), name="transaction-callbackurl"),
+    path('employerDash/',app_views.employerDash,name='employerDash'),
+
     ]
 
